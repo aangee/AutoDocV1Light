85bd1b84f8190433a8a278838e7fdef908bd2e60
aangee <gohanss4@hotmail.fr>
2022-06-04 15:46:16 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -54,7 +54,6 @@ namespace IngameScript
 
         public void Main(string argument, UpdateType updateSource)
         {
-            //Test 22222222
             if (managBlock.connectorShip != null)
             {
                 Update_P(argument);
